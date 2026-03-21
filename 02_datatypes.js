0a54605a987081220c5dee48c76ed574fce99c9b
Harshproject <103771942+Harshproject@users.noreply.github.com>
2026-03-21 16:02:26 +0000

    three

---
+++ 02_datatypes.js
@@ -0,0 +1,39 @@
+"use strict"; // treat all js code as newer version
+
+// alert('Hello, World!'); // cannot use it because we are using nodejs and not browser, it is 
+//treated as window.alert() in browser but in nodejs it is not defined, so it will throw error
+
+// Data types in JavaScript
+// 1. String
+// 2. Number
+// 3. Boolean
+// 4. Null
+// 5. Undefined
+// 6. Symbol (ES6)
+// 7. BigInt (ES2020)
+
+// String
+let name = 'Alice';
+console.log(name); // Output: Alice
+
+// Number
+let age = 30;
+console.log(age); // Output: 30
+
+// Boolean
+let isStudent = true;
+console.log(isStudent); // Output: true
+
+// Null
+let emptyValue = null;
+console.log(emptyValue); // Output: null        
+
+// Undefined is not a value but null is a value which represents no value, 
+// it is used to represent the absence of a value, it is an assignment value
+
+console.log(typeof("harsh"));
+console.log(typeof(null));
+console.log(typeof(undefined));
+
+
+
