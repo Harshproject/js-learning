e4cc1a4bf4f67a67f078caafec5058fe482022d7
Harshproject <103771942+Harshproject@users.noreply.github.com>
2026-03-26 11:33:24 +0000

    nine

--- 15_map_reduce.js
+++ 15_map_reduce.js
@@ -2,7 +2,32 @@ const arr = [1,2,3,4,5,6,7,8,10]
 
 const newarr=arr.filter((item)=>item>4);
 
-newarr.forEach((item)=>{
-    console.log(item);
+// newarr.forEach((item)=>{
+//     console.log(item);
     
-})
+// })
+
+const arr1=newarr.map((item)=>item+1000)
+
+// arr1.forEach((it)=>{
+//     console.log(it);
+    
+// })
+ console.log(newarr);
+ 
+const iniVal=0;
+//Providing in initial value to acc
+const arr2= newarr.reduce((acc, item)=>{
+    console.log(acc,item);
+    return acc+item}
+    ,iniVal);
+
+console.log(arr2);
+
+//Not providing initial value
+
+const arr3= newarr.reduce((acc, item)=>{
+    console.log(acc,item);
+    return acc+item});
+
+console.log(arr3);
---
+++ 16_DOM/dom.js
@@ -0,0 +1,17 @@
+//window->document->html->head, body....
+
+// document.getElementsByClassName("mw-heading mw-heading2").innerHTML="<p>hehe</p>"
+
+//document.querSelector(".class1")
+//const my_ul= doument.querySelector('ul')
+//const turn_green = my_ul.querySelecetor('li')
+//turn_green.style.backgroundColor="green"
+//turnGreen.style.padding="10px"
+//turnGreen.innerText="five"
+
+//If you want to see what functions does a particular selector contains then check its prototype
+
+//There are two types of list in window HTML collection and nodeList which behaves quite similar to normal array
+//But HTML collection doesnt contains foreach method so instead we will do this
+//Array.from(htmlcollectionlist)
+//Now this a normal array
